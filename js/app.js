b3f30afdbbc4716a605a9b5ddb59add40cf3b3a6
SabrinaVillalobos <wafflish@gmail.com>
2017-12-05 01:17:46 -0300

    till map done but still need to style it

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ $('#mainNav').affix({
     top: 100
   }
 })
+
+function myMap() {
+    var mapCanvas = document.getElementById("mymap");
+    var mapOptions = {
+        center: new google.maps.LatLng(-10, -78),
+        zoom: 3
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
