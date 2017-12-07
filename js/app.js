7ea7b98c805f27778c39c9e65abce6153f215d8a
SabrinaVillalobos <wafflish@gmail.com>
2017-12-07 02:13:12 -0300

    added sign in modalup

--- js/app.js
+++ js/app.js
@@ -5,12 +5,23 @@ $('#mainNav').affix({
     top: 100
   }
 })
-// Get the modal
+/*// log in modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
+// al hacer click fuera del modal se cierra
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+// sign in modal
+var modal2 = document.getElementById('id02');
+
+// al hacer click fuera del modal se cierra
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+*/
